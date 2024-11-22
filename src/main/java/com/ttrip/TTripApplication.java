fc18d813a90acc1786fba27bb6e9fe37e1d8e992
junnghyun <ajhajh1005@naver.com>
2024-11-22 10:05:10 +0900

    setting: 프로젝트 초기설정

---
+++ src/main/java/com/ttrip/TTripApplication.java
@@ -0,0 +1,13 @@
+package com.ttrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttrip/TTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
