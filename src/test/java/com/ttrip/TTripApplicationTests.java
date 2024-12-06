3d5c89804c49126e51f6c2ce5623d5c653d4153f
junnghyun <ajhajh1005@naver.com>
2024-12-06 09:39:19 +0900

    setting: project initial setup

--- src/test/java/com/ttrip/TTripApplicationTests.java
+++ src/test/java/com/ttrip/TTripApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TTripApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
