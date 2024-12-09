ba8915e8d62c8674b81378d7e520f5178bc274c6
junnghyun <ajhajh1005@naver.com>
2024-12-09 14:39:17 +0900

    chore: 시큐리티비활성화

--- src/main/java/com/ttrip/auth/config/SecurityConfig.java
+++ src/main/java/com/ttrip/auth/config/SecurityConfig.java
@@ -16,9 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
 public class SecurityConfig {
 
 //    @Autowired
