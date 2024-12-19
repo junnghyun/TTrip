0c5c48705da0b78440fc9536a703f4a5984fc3b5
2ilhwan <ilhwan0223@naver.com>
2024-12-19 13:17:26 +0900

    feat:여행코스 추가

---
+++ src/main/java/com/ttrip/tripboard/TripBoardService.java
@@ -0,0 +1,19 @@
+package com.ttrip.tripboard;
+
+import java.util.List;
+
+public class TripBoardService {
+
+	/**
+	 * 월별 베스트 코스 조회
+	 * @return
+	 */
+	public List<BestDomain> getMonthBestList(){
+		
+	}
+	
+	
+	
+	
+	
+}//class
