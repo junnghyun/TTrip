619d1ca7d1ebe86231d54215829f557f28c3dda4
junnghyun <ajhajh1005@naver.com>
2024-12-27 04:07:14 +0900

    chore: 토큰 확인

--- src/main/resources/static/common/authCheck.js
+++ src/main/resources/static/common/authCheck.js
@@ -1,16 +1,13 @@
-$(document).ready(function() {
+//  <script src="${pageContext.request.contextPath}/common/authCheck.js"></script>
+
+$(document).ready(function () {
     // 권한이 필요한 페이지에서 토큰 체크
     function checkAuthAndRedirect() {
         const token = localStorage.getItem('jwt_token');
-        if (!token) {
-            alert('로그인이 필요한 서비스입니다.');
-            window.location.href = '/login'; // 로그인 페이지로 리디렉션
-            return;
-        }
 
         // 모든 AJAX 요청에 JWT 토큰을 헤더에 추가
         $.ajaxSetup({
-            beforeSend: function(xhr) {
+            beforeSend: function (xhr) {
                 const token = localStorage.getItem('jwt_token');
                 if (token) {
                     xhr.setRequestHeader('Authorization', 'Bearer ' + token);
@@ -18,19 +15,18 @@ $(document).ready(function() {
             }
         });
 
-
         // 토큰 유효성 검증
         $.ajax({
             url: '/api/validate-token', // 토큰을 검증하는 서버 API
             type: 'GET',
-            success: function(response) {
+            success: function (response) {
                 if (response.status !== 'valid') {
                     localStorage.removeItem('jwt_token');
                     alert('세션이 만료되었습니다. 다시 로그인해주세요.');
                     window.location.href = '/login'; // 로그인 페이지로 리디렉션
                 }
             },
-            error: function() {
+            error: function () {
                 localStorage.removeItem('jwt_token');
                 alert('인증에 실패했습니다. 다시 로그인해주세요.');
                 window.location.href = '/login'; // 로그인 페이지로 리디렉션
