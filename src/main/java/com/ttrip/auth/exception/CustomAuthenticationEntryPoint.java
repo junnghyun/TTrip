e7d4b4e706656814e6f3782f085e42d598031ffd
junnghyun <ajhajh1005@naver.com>
2025-01-04 06:23:42 +0900

    chore: handler

--- src/main/java/com/ttrip/auth/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/ttrip/auth/exception/CustomAuthenticationEntryPoint.java
@@ -15,7 +15,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         // 인증되지 않은 사용자 (로그인 후 이용)일 때 alert 메시지를 보내고 로그인 페이지로 리디렉션
         response.setContentType("text/html; charset=UTF-8");
-        response.getWriter().println("<script>alert('로그인 후 이용해주세요'); location.href='/login';</script>");
+        response.getWriter().println("<script>alert('로그인 후 이용해주세요'); localStorage.removeItem('jwt_token'); location.href='/login';</script>");
         response.getWriter().flush();
     }
 }
